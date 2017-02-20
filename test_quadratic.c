32027ebe690bb7b45a50fcac5e2efc721ba80a27
Ben Andre <bjandre@gmail.com>
2017-02-20 13:21:53 -0700

    Add negated test for expected failure.
    Add a negated test for an expected failure, cleanup logic in real roots test.
    
    Testing:
      macOS: pass

--- test_quadratic.c
+++ test_quadratic.c
@@ -32,16 +32,22 @@ void test_imaginary_roots(void **state)
   assert_int_equal(status, QUAD_ERROR_imaginary_roots);
 }
 
-void test_complex_roots(void **state)
+void test_complex_roots_skip(void **state)
 {
   skip(); // complex roots have not been implemented yet.
+  assert_int_equal(0, 1);
+}
+
+void test_complex_roots_xfail(void **state)
+{
+  // complex roots have not been implemented yet, expected failure
   quad_coeffs_t coeffs;
   coeffs.a = 1.0;
   coeffs.b = 1.0;
   coeffs.c = 1.0;
   quad_roots_t roots;
   enum quadratic_status status = solve_quadratic(coeffs, &roots);
-  assert_int_equal(status, QUAD_SUCCESS);
+  assert_int_not_equal(status, QUAD_SUCCESS);
 }
 
 void test_real_roots(void **state)
@@ -53,8 +59,10 @@ void test_real_roots(void **state)
   quad_roots_t roots;
   enum quadratic_status status = solve_quadratic(coeffs, &roots);
   assert_int_equal(status, QUAD_SUCCESS);
-  assert_true(fabs(roots.root_1 - 2.0) < quadratic_epsilon);
-  assert_true(fabs(roots.root_2 + 0.5) < quadratic_epsilon);
+  double expected_root_1 = 2.0;
+  double expected_root_2 = -0.5;
+  assert_true(fabs(roots.root_1 - expected_root_1) < quadratic_epsilon);
+  assert_true(fabs(roots.root_2 - expected_root_2) < quadratic_epsilon);
 }
 
 int main(int argc, char **argv)
@@ -63,7 +71,8 @@ int main(int argc, char **argv)
     cmocka_unit_test(test_a_equal_0),
     cmocka_unit_test(test_real_roots),
     cmocka_unit_test(test_imaginary_roots),
-    cmocka_unit_test(test_complex_roots),
+    cmocka_unit_test(test_complex_roots_skip),
+    cmocka_unit_test(test_complex_roots_xfail),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
